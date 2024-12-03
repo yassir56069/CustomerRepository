f8b321cff19c62592ee084efa80ae1ba7d704d5a
yassir56069 <hoossanbukshyassir@gmail.com>
2024-12-03 00:23:19 -0800

    🧵 Add DBContext to build services

--- CustomerRepo/Program.cs
+++ CustomerRepo/Program.cs
@@ -1,4 +1,6 @@
 using CustomerRepo.Components;
+using CustomerRepo.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
